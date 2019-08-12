ee382b2e4b5b7d2bd7b9997cd8b47ecb20168aa8
wushanze <791552630@qq.com>
2019-08-12 15:08:29 +0800

    init rep

---
+++ src/main/java/com/wszstudy/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.wszstudy.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String Hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+
+
+        return "greeting";
+
+
+    }
+}
