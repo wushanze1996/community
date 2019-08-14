82bdeafb318c29913d319af49e76920199ae3b12
wushanze <791552630@qq.com>
2019-08-14 11:08:31 +0800

    Session的学习，以及th:if进行页面显示

--- src/main/java/com/wszstudy/community/controller/AuthorizeController.java
+++ src/main/java/com/wszstudy/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -20,7 +22,7 @@ public class AuthorizeController {
     @Value("${github.redirect_uri}")
     String redirect_uri;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(client_id);
         accessTokenDTO.setClient_secret(client_secret);
@@ -29,9 +31,15 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_uri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
+        if(user != null)
+        {
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
 
-        return "index";
+
 
 
     }
