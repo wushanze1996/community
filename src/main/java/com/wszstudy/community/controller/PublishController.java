4f586b0219f89292510968b6b65a43619d5eb57a
wushanze <791552630@qq.com>
2019-08-16 15:43:43 +0800

    实现了发送话题的功能

---
+++ src/main/java/com/wszstudy/community/controller/PublishController.java
@@ -0,0 +1,82 @@
+package com.wszstudy.community.controller;
+
+import com.wszstudy.community.entity.Question;
+import com.wszstudy.community.entity.User;
+import com.wszstudy.community.mapper.QuestionMapper;
+import com.wszstudy.community.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+    @Autowired
+    QuestionMapper questionMapper;
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish(){
+
+        return "publish";
+
+    }
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag, HttpServletRequest request,Model model){
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        if (title==null || title=="")
+        {
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if (description==null || description=="")
+        {
+            model.addAttribute("error","描述不能为空");
+            return "publish";
+        }
+        if (tag==null || tag =="")
+        {
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+        Cookie[] cookies = request.getCookies();
+        User user = null;
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("token")){
+                    String cookieValue = cookie.getValue();
+                    user = userMapper.findByToken(cookieValue);
+                    if (user!=null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+            if (user==null){
+                model.addAttribute("error","用户未登录");
+                return "publish";
+
+            }
+
+        Question question = new Question();
+        question.setDescription(description);
+        question.setTitle(title);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmt_create(System.currentTimeMillis());
+        question.setGmt_modified(question.getGmt_create());
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+}
+
---
+++ src/main/java/com/wszstudy/community/entity/Question.java
@@ -0,0 +1,70 @@
+package com.wszstudy.community.entity;
+public class Question {
+    private int id;
+    private String title;
+    private String description;
+    private Long gmt_create;
+    private Long gmt_modified;
+
+    public Long getGmt_create() {
+        return gmt_create;
+    }
+
+    public void setGmt_create(Long gmt_create) {
+        this.gmt_create = gmt_create;
+    }
+
+    public Long getGmt_modified() {
+        return gmt_modified;
+    }
+
+    public void setGmt_modified(Long gmt_modified) {
+        this.gmt_modified = gmt_modified;
+    }
+
+    private int creator;
+    private String tag;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public int getCreator() {
+        return creator;
+    }
+
+    public void setCreator(int creator) {
+        this.creator = creator;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
